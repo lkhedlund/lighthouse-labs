9fbdbdab3f0e57867e7e24e3573e92eb090dea3e
lkhedlund <lkhedlund@gmail.com>
2015-09-29 20:12:40 -0700

    Completed HTML/CSS/JS assignment.

--- prep course/your favourite recipe/script.js
+++ prep course/your favourite recipe/script.js
@@ -1,1 +1,9 @@
-
+var main = function () {
+    $('#directions li').prepend('<input type="checkbox">');
+    $('#ingredients li').click(function() {
+        $(this).wrap('<strike>')
+    });
+    $('#text_only').click(function() {
+        $('img').toggle();
+    });
+};
