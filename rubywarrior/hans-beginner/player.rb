908ad2071291e3277e698848ade10d04778dad25
lkhedlund <lkhedlund@gmail.com>
2015-10-21 14:27:59 -0700

    Part way through level 6. Need rest bug.

--- rubywarrior/hans-beginner/player.rb
+++ rubywarrior/hans-beginner/player.rb
@@ -1,22 +1,48 @@
 class Player
+  # BUGS: Problems with rest method acting appropriately.
+  MIN_HEALTH = 7
+
   def player_stats(warrior)
     puts "Health: #{warrior.health}"
+    puts "Walking #{@direction}"
   end # player_stats end
 
   def play_turn(warrior)
+    @health ||= warrior.health
+    @direction ||= :forward
     player_stats(warrior)
-    # Warrior turn code
-    if warrior.feel.captive?
-      warrior.rescue!
-    elsif warrior.feel.enemy?
-      warrior.attack!
-    else
-      if (warrior.health < 20) && !(warrior.health < @health)
+    # Outer if is warrior feel. Determines empty, captive or attack.
+    if warrior.feel(@direction).wall?
+      @direction = :forward
+    elsif warrior.feel(@direction).empty?
+      # Inner loop is rest, flee, and walk conditions
+      if should_flee?(warrior)
+        @direction = :backward
+        warrior.walk!(@direction)
+      elsif need_rest?(warrior)
         warrior.rest!
-      elsif
-        warrior.walk!
+      else
+        warrior.walk!(@direction)
       end
+    elsif warrior.feel(@direction).captive?
+      warrior.rescue!(@direction)
+    else
+      warrior.attack!
     end
     @health = warrior.health
   end # play_turn end
+
+  private
+
+  def need_rest?(warrior)
+    # Determines whether the player needs a rest, and takes one.
+    low_health = warrior.health < 20
+    not_taking_damage = !(warrior.health < @health)
+    return low_health && not_taking_damage
+  end #need_rest end
+
+  def should_flee?(warrior)
+    # Determines whether the warrior should run away.
+    return warrior.health < MIN_HEALTH
+  end
 end # class end
