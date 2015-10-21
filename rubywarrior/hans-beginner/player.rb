725c2086f4f5c7d8907fc1382f1d27428a4805b8
lkhedlund <lkhedlund@gmail.com>
2015-10-21 11:49:07 -0700

    Ruby warrior level 5 completed.

--- rubywarrior/hans-beginner/player.rb
+++ rubywarrior/hans-beginner/player.rb
@@ -1,10 +1,22 @@
 class Player
+  def player_stats(warrior)
+    puts "Health: #{warrior.health}"
+  end # player_stats end
+
   def play_turn(warrior)
-    # add your code here
-    if warrior.feel.empty?
-      warrior.walk!
-    else
+    player_stats(warrior)
+    # Warrior turn code
+    if warrior.feel.captive?
+      warrior.rescue!
+    elsif warrior.feel.enemy?
       warrior.attack!
+    else
+      if (warrior.health < 20) && !(warrior.health < @health)
+        warrior.rest!
+      elsif
+        warrior.walk!
+      end
     end
-  end
-end
+    @health = warrior.health
+  end # play_turn end
+end # class end
