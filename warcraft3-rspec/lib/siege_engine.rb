7a5b4542fd5d03a5bed5cb36a3275d3885c22b91
lkhedlund <lkhedlund@gmail.com>
2015-11-04 14:46:37 -0800

    Finished last rspec condition and test.

--- warcraft3-rspec/lib/siege_engine.rb
+++ warcraft3-rspec/lib/siege_engine.rb
@@ -7,6 +7,8 @@ class SiegeEngine < Unit
   def attack!(enemy)
     if enemy.instance_of? Barracks
       enemy.damage(@attack_power * 2)
+    elsif enemy.instance_of? SiegeEngine
+      super
     end
   end
 
