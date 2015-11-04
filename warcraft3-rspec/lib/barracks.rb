ded85495a02423fc78b9c31881fe4a8f50f284ae
lkhedlund <lkhedlund@gmail.com>
2015-11-03 20:30:40 -0800

    Completed half of the tests

--- warcraft3-rspec/lib/barracks.rb
+++ warcraft3-rspec/lib/barracks.rb
@@ -1,6 +1,22 @@
 class Barracks
+  attr_accessor :gold, :food
+
+  def initialize
+    @gold = 1000
+    @food = 80
+  end
+
+  def train_footman
+    if can_train_footman?
+      @gold -= 135
+      @food -= 2
+      Footman.new
+    else
+      return nil
+    end
+  end
 
   def can_train_footman?
-    true
+    @gold >= 135 && @food >= 2
   end
 end
