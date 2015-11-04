551d286a7dbf73d96ebace4686354e8623cfd261
lkhedlund <lkhedlund@gmail.com>
2015-11-03 21:05:27 -0800

    All tests complete.

--- warcraft3-rspec/lib/barracks.rb
+++ warcraft3-rspec/lib/barracks.rb
@@ -11,12 +11,28 @@ class Barracks
       @gold -= 135
       @food -= 2
       Footman.new
-    else
-      return nil
     end
   end
 
   def can_train_footman?
-    @gold >= 135 && @food >= 2
+    # instance variables causing error in rspec test
+    # cannot change test, but know that these
+    # should be instance variables.
+    gold >= 135 && food >= 2
+  end
+
+  def train_peasant
+    if can_train_peasant?
+      @gold -= 90
+      @food -= 5
+      Peasant.new
+    end
+  end
+
+  def can_train_peasant?
+    # instance variables causing error in rspec test
+    # cannot change test, but know that these
+    # should be instance variables.
+    gold >= 90 && food >= 5
   end
 end
--- warcraft3-rspec/lib/footman.rb
+++ warcraft3-rspec/lib/footman.rb
@@ -5,13 +5,4 @@ class Footman < Unit
   def initialize
     super(60, 10)
   end
-
-  def attack!(enemy)
-    enemy.damage(10)
-  end
-
-  def damage(attack_power)
-    @health_points -= attack_power
-  end
-
 end
