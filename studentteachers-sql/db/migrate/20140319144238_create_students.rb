ebf8c62857c4787f19f01a1d012c92ee79e9ec1b
lkhedlund <lkhedlund@gmail.com>
2015-11-12 21:11:48 +0000

    StudentTeachers database migrated over

---
+++ studentteachers-sql/db/migrate/20140319144238_create_students.rb
@@ -0,0 +1,19 @@
+class CreateStudents < ActiveRecord::Migration
+
+  def change
+    # Add code to create the table here
+    # HINT: check out ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      # column definitions go here
+      # Use the AR migration guide for syntax reference
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :email
+      t.string :phone
+      t.date :birthday
+      t.timestamps null: false
+    end
+  end
+
+end
