2093b1bb6a90461ced812a2095785ba3b403e70a
lkhedlund <lkhedlund@gmail.com>
2015-11-13 03:46:24 +0000

    Student rspec validations complete

--- studentteachers-sql/app/models/student.rb
+++ studentteachers-sql/app/models/student.rb
@@ -3,7 +3,14 @@ class Student < ActiveRecord::Base
   belongs_to :teacher
 
   validates :email, uniqueness: true
-  validates :birthday, numericality: { minimum: Date.today - 3.years}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validate :cannot_be_too_young
+
+  def cannot_be_too_young
+    if birthday >= Date.today - 3.years
+      errors.add(:birthday, "can't be too young")
+    end
+  end
 
   def name
     first_name + " " + last_name
