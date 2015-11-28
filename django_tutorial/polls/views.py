6029920031f9180defa34dff2d4686063c58600d
lkhedlund <lkhedlund@gmail.com>
2015-11-28 14:57:58 -0800

    namespaced polls and removed hardcoded url

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -1,31 +1,14 @@
-# from django.http import Http404
-# This is a shortcut for loading templates, filling contexts, and returning HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-# from django.template import RequestContext, loader
 
 from .models import Question
 
-# Create your views here.
 def index(request):
-    # displays the last 5 polls in the system
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = { 'latest_question_list': latest_question_list}
-    # output = ', '.join([p.question_text for p in latest_question_list])
-    # return HttpResponse(output)
-    # return HttpResponse(template.render(context))
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    # version 1
-    # response = "You're looking at question %s."
-    # return HttpResponse(response % question_id)
-    # version 2
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # version 3
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
