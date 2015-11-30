d1a4ff31aa4fc2d457fefb28de4c74520154b9ec
lkhedlund <lkhedlund@gmail.com>
2015-11-30 22:47:57 +0000

    Basic betting game complete

--- betting_game/js/betting_game.js
+++ betting_game/js/betting_game.js
@@ -1,31 +1,30 @@
 // JS Betting game
+RANDOM_CAP = 10;
+bankroll = 40
 
 var BettingGame = BettingGame || {};
 
 // BettingGame.init = function() {};
 
 BettingGame.run = function() {
-  var bankroll = 100;
-  RANDOM_CAP = 10;
-  // var bet = "";
+  do {
+    BettingGame.process_bet();
+    var quit = prompt("Would you like to quit? (Yn)")
+  } while (bankroll >= 5 && quit != "Y");
+};
 
+BettingGame.process_bet = function() {
   alert("Your bankroll is " + bankroll);
-
   do {
     var bet = parseInt(prompt('Bet $5 or $10? '), 10);
   } while ((bet != 5) && (bet != 10));
 
-  console.log("DEBUG:" + bet)
   var rand_num = Math.floor(Math.random() * (RANDOM_CAP + 1));
   console.log("DEBUG:" + rand_num);
   var guess = prompt("Please choose a number between 1 and 10");
   if (guess == rand_num) {
-    console.log("DEBUG: guess == rand");
     bankroll += bet;
   } else {
-    console.log("DEBUG: guess != rand");
     bankroll -= bet;
   };
-  console.log("DEBUG:" + bankroll);
-
 };
