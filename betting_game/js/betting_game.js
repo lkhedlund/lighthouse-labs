e71719e43c29d049b8ffd39f053fa71a541435ba
lkhedlund <lkhedlund@gmail.com>
2015-11-30 23:14:36 +0000

    Basic game working.

--- betting_game/js/betting_game.js
+++ betting_game/js/betting_game.js
@@ -7,24 +7,33 @@ var BettingGame = BettingGame || {};
 // BettingGame.init = function() {};
 
 BettingGame.run = function() {
-  do {
-    BettingGame.process_bet();
-    var quit = prompt("Would you like to quit? (Yn)")
-  } while (bankroll >= 5 && quit != "Y");
+  BettingGame.refresh();
+
+  var $display_bankroll = $('#question');
+  $display_bankroll.append("Your current bankroll is " + bankroll);
 };
 
-BettingGame.process_bet = function() {
-  alert("Your bankroll is " + bankroll);
-  do {
-    var bet = parseInt(prompt('Bet $5 or $10? '), 10);
-  } while ((bet != 5) && (bet != 10));
+BettingGame.refresh = function() {
+  $('#question').html('');
+};
 
-  var rand_num = Math.floor(Math.random() * (RANDOM_CAP + 1));
-  console.log("DEBUG:" + rand_num);
-  var guess = prompt("Please choose a number between 1 and 10");
-  if (guess == rand_num) {
-    bankroll += bet;
-  } else {
-    bankroll -= bet;
+BettingGame.place_bet = function(id_val) {
+  var $submit = $('#submit');
+
+  if (id_val == 'submit') {
+    do {
+      var bet = parseInt(prompt('Bet $5 or $10? '), 10);
+    } while ((bet != 5) && (bet != 10));
+
+    var rand_num = Math.floor(Math.random() * (RANDOM_CAP + 1));
+    console.log("DEBUG:" + rand_num);
+    var guess = prompt("Please choose a number between 1 and 10");
+    if (guess == rand_num) {
+      bankroll += bet;
+    } else {
+      bankroll -= bet;
+    };
+    BettingGame.run();
   };
+
 };
