5d1038e25792b95ae8bc151003edb4cf9197e11f
Lars Hedlund <lkhedlund@gmail.com>
2015-12-07 22:54:47 +0000

    Learn Node you exercise 7

---
+++ node_learn_you/07_http_client.js
@@ -0,0 +1,17 @@
+var http = require('http');
+URL = process.argv[2];
+
+http.get(URL, function(response) {
+  response.on('data', function (data) {
+    console.log(data.toString('utf8'));
+  });
+});
+
+// provided solution
+
+var http = require('http')
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+})
